85d09a70e50eb9936a4062b26cd308a9b7675233
tanu-logiciel <tanu.baweja@logicielsolutions.co.in>
2017-09-12 14:18:26 +0530

    setup commit

--- footer.php
+++ footer.php
@@ -1,2 +1,5 @@
+		<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="assets/js/custom.js"></script>
 	</body>
 </html>
--- header.php
+++ header.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+    	<title>ERoofPro</title>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="" crossorigin="anonymous">
+        <!-- CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/style.css" crossorigin="anonymous">
     </head>
     <body>
     
