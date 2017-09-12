fb0fae1dab0b95f5a33d65f16c9a4e59476d39d7
tanu-logiciel <tanu.baweja@logicielsolutions.co.in>
2017-09-12 15:29:41 +0530

    signup screen basic layout

---
+++ signup.php
@@ -0,0 +1,12 @@
+<?php include("header.php"); ?>
+	<div class="body-bg-wrapper">
+		<div class="bg-overlay"></div>
+		<div class="container content-container">
+			<div class="middle-container">
+				<div class="middle-container-inner">
+					dfgd
+				</div>
+			</div>
+		</div>
+	</div>
+<?php include("footer.php"); ?>
