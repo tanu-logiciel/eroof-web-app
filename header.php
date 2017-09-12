4ea48e5c1ed7908b03328a65aa86accbd54b414b
tanu-logiciel <tanu.baweja@logicielsolutions.co.in>
2017-09-12 15:44:35 +0530

    font family included

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 
         <!-- CSS -->
         <link rel="stylesheet" href="assets/css/bootstrap.min.css" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
         <link rel="stylesheet" href="assets/css/font-awesome.min.css" crossorigin="anonymous">
         <link rel="stylesheet" href="assets/css/style.css" crossorigin="anonymous">
     </head>
