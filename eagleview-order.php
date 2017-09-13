e95facbbb3a4f01065f66b7595f2f8c3b8783612
tanu-logiciel <tanu.baweja@logicielsolutions.co.in>
2017-09-13 18:49:45 +0530

    eagleview order screen setup

--- eagleview-order.php
+++ eagleview-order.php
@@ -7,12 +7,19 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-12 col-sm-12 text-center">
+		<div class="col-md-12 col-sm-12">
 			<div class="map-container">
 				<img src="assets/images/map.jpg" alt="map">
 			</div>
 			<div class="ep-bottom-banner">
-				
+				<div class="row">
+					<div class="col-md-8 col-sm-7">
+						fguydfgfhg
+					</div>
+					<div class="col-md-4 col-sm-5 text-center">
+						fgydfyrgfy	
+					</div>
+				</div>
 			</div>
 			<div class="extra-spacer"></div>
 		</div>
