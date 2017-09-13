8bbed93d6dd2167ccc5d0b3ccc753a0d1e4960e9
tanu-logiciel <tanu.baweja@logicielsolutions.co.in>
2017-09-13 17:14:38 +0530

    welcome screen

---
+++ scripts.php
@@ -0,0 +1,3 @@
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/custom.js"></script>
---
+++ welcome.php
@@ -0,0 +1,23 @@
+<?php include('main-header.php'); ?>
+
+		<div class="row">
+			<div class="col-md-12 col-sm-12 text-center">
+				<h1 class="big-title ep-heading">Welcome, Test Data</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 col-sm-12 text-center">
+				<div class="map-container">
+					<img src="assets/images/map.jpg" alt="map">
+				</div>
+				<p class="map-note text-center text-lead">Note - Kindly drag the pin to right location before you place the estimate order</p>
+				<div class="order-estimate-module">
+					<span class="est-title">order free eagleview estimate</span>
+					<span class="est-sub-title">Estimate from satellite images</span>
+				</div>
+				<div class="extra-spacer"></div>
+			</div>
+		</div>
+
+
+<?php include('main-footer.php'); ?>
