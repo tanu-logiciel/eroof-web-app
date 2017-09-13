d09397b6b3429ee92ef9180a68179e3f3d1bf842
jyoti <jyoti.joshi@logicielsolutions.co.in>
2017-09-12 18:18:48 -0700

    login-page

--- login.php
+++ login.php
@@ -2,38 +2,40 @@
 <div class="body-bg-wrapper">
 	<div class="bg-overlay"></div>
 	<div class="container content-container">
-		<div class="middle-container">
-			<div class="middle-container-inner login-outer-wrap">
-				<div class="row margin0">
-					<div class="col-sm-12 login-form-space">
-						<img src="assets/images/Logo.png" alt="logo">
+		<section>
+			<div class="middle-container">
+				<div class="middle-container-inner login-outer-wrap">
+					<div class="row margin0">
+						<div class="col-sm-12 login-form-space">
+							<img src="assets/images/Logo.png" alt="logo">
+						</div>
 					</div>
+					<!-- Input Fields Start- -->
+					<div class="row margin0 fields-wrap">
+						<div class="col-sm-12">
+							<div class="input-fields text-left">
+								<input type="text" placeholder="Username / Email" />
+							</div>
+						</div>
+					</div>
+					<div class="row margin0 fields-wrap-pass">
+						<div class="col-sm-12">
+							<div class="input-fields text-left">
+								<input type="Password" placeholder="Password" />
+							</div>
+						</div>
+					</div>
+					<div class="row margin0">
+						<div class="col-sm-12 forgot-pass text-right">
+							<p>Forgot Password?</p>
+						</div>
+					</div>
+					<button type="button" class="btn btn-secondary btn-lg sign-in">Sign In</button>
 				</div>
-				<!-- Input Fields Start- -->
-				<div class="row margin0 fields-wrap">
-					<div class="col-sm-12">
-					<div class="input-fields text-left">
-						<input type="text" placeholder="Username / Email" />
-					</div>
-					</div>
-				</div>
-				<div class="row margin0 fields-wrap-pass">
-					<div class="col-sm-12">
-					<div class="input-fields text-left">
-						<input type="text" placeholder="Password" />
-					</div>
-					</div>
-				</div>
-				<div class="row margin0">
-					<div class="col-sm-12 forgot-pass text-right">
-						<p>Forgot Password?</p>
-					</div>
-				</div>
-				<button type="button" class="btn btn-secondary btn-lg sign-in">Sign In</button>
+				<!-- /Input Fields- -->
+				<p class="outer-line-login">Don't have an account?<span> Sign Up</span></p>
 			</div>
-			<!-- /Input Fields- -->
-			<p class="outer-line-login">Don't have an account?<span> Sign Up</span></p>
-		</div>
+		</section>
 	</div>
 </div>
 <?php include("footer.php"); ?>
