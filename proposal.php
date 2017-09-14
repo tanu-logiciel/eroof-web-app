db3be8da1d79b444e7c4cc8a5128230df0b46691
jyoti <jyoti.joshi@logicielsolutions.co.in>
2017-09-14 15:20:58 -0700

    proposal-page

---
+++ proposal.php
@@ -0,0 +1,51 @@
+<?php include('header.php'); ?>
+<?php include('main-header.php'); ?>
+
+<div class="row">
+	<div class="col-md-12 col-sm-12 text-center">
+		<h1 class="big-title ep-heading">Choose Proposal</h1>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-4 col-sm-4">
+		<div class="image-section-outer">
+			<img src="assets/images/best-home.jpg" class="img-responsive" alt="best-home">
+				<div class="designer-outer-text">
+					<p><span>Best</span> <span>- Designer</span></p>
+				</div>
+				<div class="designer-outer-icon">
+					<img src="assets/images/check-icon.png" alt="check-icon">
+				</div>
+			<div class="image-section-inner">
+				<!-- <div class="designer-bottom-text">
+					<p>5100 Hwy 33 & 34, </p>
+					<p>Farmingdale, NJ 07727</p>
+				</div> -->
+			</div>
+			<!-- <div class="price-outer">
+				<p>$3950</p>
+			</div> -->
+		</div>
+	</div>
+	<div class="col-sm-4 col-md-4">
+		<div class="image-section-outer">
+			<img src="assets/images/best-home.jpg" class="img-responsive" alt="best-home">
+			<div class="image-section-inner">
+				<p>5100 Hwy 33 & 34, </p>
+				<p>Farmingdale, NJ 07727</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-4 col-md-4">
+		<div class="image-section-outer">
+			<img src="assets/images/best-home.jpg" class="img-responsive" alt="best-home">
+			<div class="image-section-inner">
+				<p>5100 Hwy 33 & 34, </p>
+				<p>Farmingdale, NJ 07727</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include('main-footer.php'); ?>
+<?php include('footer.php'); ?>
