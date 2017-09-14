4d4b93b38e098569f4b15540b37bd72166699a0e
tanu-logiciel <tanu.baweja@logicielsolutions.co.in>
2017-09-14 12:47:32 +0530

    estimate setup

---
+++ my-estimates.php
@@ -0,0 +1,29 @@
+<?php include('header.php'); ?>
+<?php include('main-header.php'); ?>
+
+	<div class="row">
+		<div class="col-md-12 col-sm-12 text-center">
+			<h1 class="ep-heading">My Estimates</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4 col-sm-4">
+			<div class="ep-estimate-col">
+				ghryguyrfgrfyyh
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-4">
+			<div class="ep-estimate-col">
+				ghryguyrfgrfyyh
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-4">
+			<div class="ep-estimate-col">
+				ghryguyrfgrfyyh
+			</div>
+		</div>
+	</div>
+	<div class="extra-spacer"></div>
+
+<?php include('main-footer.php'); ?>
+<?php include('footer.php'); ?>
