d0e4e00a939afa26f151134a81d761a9ab7cd0d7
jyoti <jyoti.joshi@logicielsolutions.co.in>
2017-09-14 19:13:49 -0700

    signup ui

---
+++ demo.php
@@ -0,0 +1,11 @@
+<?php include("header.php"); ?>
+<div class="body-bg-wrapper">
+	<div class="bg-overlay"></div>
+	<div class="container content-container">
+		<div class="middle-container">
+			<div class="middle-container-inner">
+			</div>
+		</div>
+	</div>
+</div>
+<?php include("footer.php"); ?>
