d451aa6c7f1a16961d1124dc9fc40f3de9b2117b
jyoti <jyoti.joshi@logicielsolutions.co.in>
2017-09-15 17:16:15 -0700

    proposal

---
+++ old-proposal.php
@@ -0,0 +1,103 @@
+<?php include('header.php'); ?>
+<?php include('main-header.php'); ?>
+
+<div class="row">
+	<div class="col-md-12 col-sm-12 text-center">
+		<h1 class="big-title ep-heading">Choose Proposal</h1>
+	</div>
+</div>
+<!--Image Section- -->
+<div class="row">
+	<div class="col-md-4 col-sm-4">
+		<div class="image-section-outer">
+			<div class="image-overlay"></div>
+			<img src="assets/images/best-home.jpg" class="img-responsive" alt="best-home">
+			<div class="designer-outer-text">
+				<p><span>Best</span> <span>- Designer</span></p>
+			</div>
+			<div class="designer-outer-icon">
+				<img src="assets/images/check-icon.png" class="check-icon" alt="check-icon">
+			</div>
+			<div class="image-section-bottom">
+				<p>5100 Hwy 33 & 34, </p>
+				<p>Farmingdale, NJ 07727</p>
+			</div>
+			<div class="price-outer">
+				<p>$3950</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-4">
+		<div class="image-section-outer">
+			<div class="image-overlay"></div>
+			<img src="assets/images/better-home.jpg" class="img-responsive" alt="better-home">
+			<div class="designer-outer-text designer-better-text">
+				<p><span>Better</span> <span>- Designer</span></p>
+			</div>
+			<div class="designer-outer-icon">
+				<img src="assets/images/check-icon.png" class="check-icon" alt="check-icon">
+			</div>
+			<div class="image-section-bottom">
+				<p>5100 Hwy 33 & 34, </p>
+				<p>Farmingdale, NJ 07727</p>
+			</div>
+			<div class="price-outer">
+				<p>$3950</p>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-4">
+		<div class="image-section-outer">
+			<div class="image-overlay"></div>
+			<img src="assets/images/good-home.jpg" class="img-responsive" alt="good-home">
+			<div class="designer-outer-text designer-good-text">
+				<p><span>Good</span> <span>- Designer</span></p>
+			</div>
+			<div class="designer-outer-icon">
+				<img src="assets/images/check-icon.png" class="check-icon" alt="check-icon">
+			</div>
+			<div class="image-section-bottom">
+				<p>5100 Hwy 33 & 34, </p>
+				<p>Farmingdale, NJ 07727</p>
+			</div>
+			<div class="price-outer">
+				<p>$3950</p>
+			</div>
+		</div>
+	</div>
+</div>
+<!--/Image Section- -->
+<div class="row">
+	<div class="col-md-12 col-sm-12 specifications">
+		<h2>Specifications:</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<ul>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+		</ul>
+		<ul>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+		</ul>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-12 col-sm-12 text-center">
+		<div class="specification-banner-btn">
+		<a href="#" class="btn custom-btn btn-gray">Detailed Specs</a>
+			<a href="#" class="btn custom-btn btn-red">Choose Contractor</a>
+		</div>
+	</div>
+</div>
+<?php include('main-footer.php'); ?>
+<?php include('footer.php'); ?>
--- scripts.php
+++ scripts.php
@@ -1,3 +1,4 @@
 <script type="text/javascript" src="assets/js/jquery.min.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="assets/js/custom.js"></script>
+
